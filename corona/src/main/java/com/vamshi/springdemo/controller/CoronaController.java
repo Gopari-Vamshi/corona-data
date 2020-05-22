c9a7ccda977d4470e2ff4ac8654a48a675abfb83
Vamshi Gopari <47558150+Gopari-Vamshi@users.noreply.github.com>
2020-05-22 11:47:31 +0530

    Add files via upload

---
+++ corona/src/main/java/com/vamshi/springdemo/controller/CoronaController.java
@@ -0,0 +1,26 @@
+package com.vamshi.springdemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.vamshi.springdemo.entity.Details;
+import com.vamshi.springdemo.service.CoronaService;
+
+
+@Controller
+public class CoronaController {
+
+	@Autowired
+	private CoronaService coronaService;
+	
+	@GetMapping("/CoronaData")
+	public String getDataFromApi(Model theModel)throws Exception {
+
+		Details theDetails =coronaService.getDetails();
+		
+		theModel.addAttribute("worlddetails", theDetails);
+	
+		return "corona-form";
+	}
+}
